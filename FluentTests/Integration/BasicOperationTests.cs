658520a13ee5fc2d0ac3b92449478760f1f4a46e
Justin Self <justn.self@gmail.com>
2013-12-23 13:38:12 -0600

    added integration tests for waitable things

--- FluentTests/Integration/BasicOperationTests.cs
+++ FluentTests/Integration/BasicOperationTests.cs
@@ -28,9 +28,37 @@ namespace FluentTests.Integration
         [Test]
         public void ShouldGoToPageEnterTextClickButtonAndWaitForElementsToAppear()
         {
-            
+            using (var I = new FluentDriver(new FirefoxDriver(null, new FirefoxProfile { EnableNativeEvents = false })))
+            {
+                I.OpenPage(google);
+                I.EnterText("cats").Into("input[name=\"q\"]");
+                I.Expect().ValueOf("input[name=\"q\"]").ToBe("cats");
+            }
         }
 
+        [Test]
+        public void ShouldWaitForElementsToAppear()
+        {
+            using (var I = new FluentDriver(new FirefoxDriver(null, new FirefoxProfile { EnableNativeEvents = false })))
+            {
+                I.OpenPage(google);
+                I.EnterText("cats").Into("input[name=\"q\"]");
+                I.Click().On("input[name=\"btnK\"]");
+                I.Expect().ValueOf("#lst-ib").WithIn(2000).ToBe("cats");
+            }
+        }
 
+        [Test]
+        public void ShouldNotWaitForElementsToAppearAndThrow()
+        {
+            using (var I = new FluentDriver(new FirefoxDriver(null, new FirefoxProfile { EnableNativeEvents = false })))
+            {
+                I.OpenPage(google);
+                I.EnterText("cats").Into("input[name=\"q\"]");
+                I.Click().On("input[name=\"btnK\"]");
+                Action act = () => { I.Expect().ValueOf("lst-ib").ToBe("cats"); };
+                act.ShouldThrow<Exception>();
+            }
+        }
     }
 }
